9d73f736abaea4e7fd68a4f748c6c9cc8c73b23c
doantractai99 <69450029+doantractai99@users.noreply.github.com>
2021-09-03 10:33:40 +0700

    up

--- 2.JS/View/js/base/paging.js
+++
@@ -1,8 +0,0 @@
-/**
- * Hàm hiển thị số lượng nhân viên
- * Dvanh 21/07/2021
- * @param {*} number 
- */
-function updateEntityNumber(number) {
-    $("#div1-paging").text(`Hiển thị ${number}/${number} nhân viên`);
-}
